7b77647556c810100ea390ecb571eff59f81adca
Sarah Louise <slouise@live.com.my>
2015-09-10 14:50:48 +0800

    version on live server

--- d/animate.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    //$('#accordion li.content').filter(':nth-child(n+4)').addClass('hide');
-    $('#accordion li.content').addClass('hide');
-	
-	$('#accordion').on('click','li.title',function() {
-		$(this).next().slideDown(200).siblings('li.content').slideUp(200);
-		return false;
-		})
-});
--- index.php
+++ index.php
@@ -23,7 +23,6 @@ $ignore = array("photo-galleries-fauna",
 			"photo-galleries-people-of-borneo");
 			
 $c = array("about-us", 
-			"blog", 
 			"our-adventures", 
 
 			"danum-valley", 
@@ -96,11 +95,6 @@ $c = array("about-us",
 			
 			"monkey-dives",
 			"custom-trips", 
-			"photographs-of-borneo", 
-			"photo-galleries-fauna",
-			"photo-galleries-flora-of-borneo",
-			"photo-galleries-landscape-of-borneo",
-			"photo-galleries-people-of-borneo",
 			"responsible-tourism",
 			"contact-us",
 			"booking",
@@ -110,7 +104,7 @@ $c = array("about-us",
 			"our-friends",
 			"unsubscribe",
 			"how-is-sticky-rice-travel-doing",
-			"mount-kinabalu"
+			"404"
 			);
 
 /* Site Map Generator			
@@ -135,13 +129,8 @@ if($p[1] == "about-us") {
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. We're young and ambitious travel agency in Borneo bringing you infinity and Meaningful Adventures!";
 }		
 			
-if($p[1] == "blog") {
-	$t = "Blog | Meaningful Adventures with Sticky Rice Travel";
-	$d = "Book amazing borneo holidays with Sticky Rice Travel. Check out interesting write-ups and videos about our adventures in Borneo. Meaningful Adventures!";
-}	
-			
 if($p[1] == "our-adventures") {
-	$t = "Travel, Trips, Package Holidays and more in Borneo";
+	$t = "Our Adventures | Travel, Trips, Package Holidays and more in Borneo";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. We offer trips, holidays packages, tours, mountain climbing, diving and more Meaningful Adventures!";
 }
 
@@ -153,7 +142,7 @@ if($p[1] == "danum-valley") {
 }
 
 if($p[1] == "borneo-rainforest-lodge") {
-	$t = "Borneo Adventure Tourism, Sandakan Tour, Rainforest Lodge";
+	$t = "Danum Valley | Borneo Rainforest Lodge";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Stay in a 5 star lodge and experience wilderness and nature in Danum Valley. Meaningful Adventures!";
 }
 
@@ -163,22 +152,22 @@ if($p[1] == "danum-valley-field-center") {
 }
 
 if($p[1] == "danum-purut-camping") {
-	$t = "Danum Valley. Sungai Purut Exploration. Danum Expedition.";
+	$t = "Danum Valley | Sungai Purut Camping";
 	$d = "Book amazing borneo holidays with Sticky Rice. A destination with high concentration of wildlife & old forests, for nature geek. Meaningful Adventures!";
 }
 
 if($p[1] == "danum-expedition") {
-	$t = "Danum Valley. Expedition to Mt.Danum. Danum Expedition.";
+	$t = "Danum Valley | Expedition to Mt. Danum";
 	$d = "Book amazing borneo holidays with Sticky Rice. A destination with high concentration of wildlife & old forests, for nature geek. Meaningful Adventures!";
 }
 
 if($p[1] == "danum-jungle-to-luxury") {
-	$t = "Danum Valley. Wild Jungles to Luxury.";
+	$t = "Danum Valley | Wild Jungles to Luxury";
 	$d = "Book amazing borneo holidays with Sticky Rice. A destination with high concentration of wildlife & old forests, for nature geek. Meaningful Adventures!";
 }
 
 if($p[1] == "danum-explorer") {
-	$t = "Danum Valley. DVFC Explorer";
+	$t = "Danum Valley | DVFC Explorer";
 	$d = "Book amazing borneo holidays with Sticky Rice. A destination with high concentration of wildlife & old forests, for nature geek. Meaningful Adventures!";
 }
 
@@ -190,27 +179,27 @@ if($p[1] == "kinabatangan") {
 }
 
 if($p[1] == "kinabatangan-abai-homestay") {
-	$t = "Abai Homestay Sabah. Borneo Travel & Borneo Adventures";
+	$t = "Kinabatangan | Abai Homestay Sabah";
 	$d = "Book amazing borneo holidays with Sticky Rice. We encourage tourists to consider home stay arrangements while visiting Borneo. Meaningful Adventures!";
 }
 
 if($p[1] == "kinabatangan-ecocamp") {
-	$t = "Eco Camp Kinabatangan, Borneo Eco Tour Adventures";
+	$t = "Kinabatangan | Eco Camp, Borneo Eco Tour Adventures";
 	$d = "Book amazing borneo holidays with Sticky Rice. Stay at the only accommodation on Kinabatangan that is located in a forest reserve. Meaningful Adventures!";
 }
 
 if($p[1] == "kinabatangan-supu-camp") {
-	$t = "Supu Camp Kinabatangan, River Cruise in Sabah Borneo";
+	$t = "Kinabatangan | Supu Camp, River Cruise in Sabah Borneo";
 	$d = "Book amazing borneo holidays with Sticky Rice. Borneo River Cruise, forest trekking, spot wildlife, explore limestone caves & more. Meaningful Adventures";
 }
 
 if($p[1] == "kinabatangan-wetlands-resort") {
-	$t = "Kinabatangan Wetlands Resort";
+	$t = "Kinabatangan | Kinabatangan Wetlands Resort";
 	$d = "Book amazing borneo holidays with Sticky Rice. Supu Camp, Homestay, River Cruise, spot wildlife, explore limestone caves and more. Meaningful Adventures!";
 }
 
 if($p[1] == "kinabatangan-borneo-nature-lodge") {
-	$t = "Borneo Nature Lodge, Sabah Tourism, Borneo Itinerary";
+	$t = "Kinabatangan | Borneo Nature Lodge";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
@@ -222,22 +211,22 @@ if($p[1] == "sandakan-and-sepilok") {
 }
 
 if($p[1] == "ss-sepilok-sojourn") {
-	$t = "Sepilok Sojourn";
+	$t = "Sandakan & Sepilok | Sepilok Sojourn";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-libaran-turtle-island-camping") {
-	$t = "Libaran Turtle Island Camping, Sandakan Tour & Resort";
+	$t = "Sandakan & Sepilok | Libaran Turtle Island Camping";
 	$d = "Book amazing borneo holidays with Sticky Rice. It's not camping, its glamping! Visit turtle island borneo, Libaran, Sabah Island. Meaningful Adventures!";	
 }
 
 if($p[1] == "ss-orang-utan-and-turtles") {
-	$t = "Orang Utans & Turtles";
+	$t = "Sandakan & Sepilok | Orang Utans & Turtles";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-lankayan-island") {
-	$t = "Lankayan Island";
+	$t = "Sandakan & Sepilok | Lankayan Island";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
@@ -247,54 +236,54 @@ if($p[1] == "sandakan-and-sepilok-accommodation") {
 }
 
 if($p[1] == "ss-accommodation-sepilok-nature-resort") {
-	$t = "Sepilok Nature Resort";
+	$t = "Sepilok | Sepilok Nature Resort";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-accommodation-sepilok-forest-edge") {
-	$t = "Sepilok Forest Edge";
+	$t = "Sepilok | Sepilok Forest Edge";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-accommodation-paganakan-dii-tropical-retreat") {
-	$t = "Peganakan Dii Tropical Retreat, Sepilok";
+	$t = "Sepilok | Peganakan Dii Tropical Retreat";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-accommodation-sheraton-four-points") {
-	$t = "Sheraton Four Points, Sandakan";
+	$t = "Sandakan | Sheraton Four Points";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 if($p[1] == "ss-accommodation-nak-hotel") {
-	$t = "Nak Hotel, Sandakan";
+	$t = "Sandakan | Nak Hotel";
 	$d = "Book amazing borneo holidays with Sticky Rice. See feeding of Sepilok Orang Utan, kinabatangan river cruise, in search of wildlife. Meaningful Adventures";
 }
 
 // CROCKER RANGE PARK
 
 if($p[1] == "crocker-range-park") {
-	$t = "Croker Range Park, Borneo Adventure";
+	$t = "Crocker Range Park, Borneo Adventure";
 	$d = "Book amazing borneo holidays with Sticky Rice. Adventurous 2 day trek in Crocker Range. Enjoy beautiful views, plants and birds. Meaningful Adventures!";
 }
 
 if($p[1] == "crocker-range-park-gunung-alab-hike") {
-	$t = "Gunung Alab Hike, Borneo Adventure";
+	$t = "Crocker Range Park | Gunung Alab Hike";
 	$d = "Book amazing borneo holidays with Sticky Rice. Adventurous 2 day trek in Crocker Range. Enjoy beautiful views, plants and birds. Meaningful Adventures!";
 }
 
 if($p[1] == "crocker-range-park-mt-trus-madi") {
-	$t = "Mt Trus Madi Trek, Adventure Kota Kinabalu Itinerary";
+	$t = "Crocker Range Park | Mt Trus Madi Trek";
 	$d = "Book amazing borneo holidays with Sticky Rice. Adventurous trekking in the Crocker National Park. Enjoy beautiful views and more. Meaningful Adventures!";
 }
 
 if($p[1] == "crocker-range-park-day-trip") {
-	$t = "Crocker Range Park, Adventure Kota Kinabalu Itinerary";
+	$t = "Crocker Range Park | Crocker Range Park";
 	$d = "Book amazing borneo holidays with Sticky Rice. Adventurous trekking in the Crocker National Park. Enjoy beautiful views and more. Meaningful Adventures!";
 }
 
 if($p[1] == "crocker-range-park-sinurambi-bed-and-breakfast") {
-	$t = "Sinurambi Bed & Breakfast, Adventure Kota Kinabalu Itinerary";
+	$t = "Crocker Range Park | Sinurambi Bed & Breakfast";
 	$d = "Book amazing borneo holidays with Sticky Rice. Adventurous trekking in the Crocker National Park. Enjoy beautiful views and more. Meaningful Adventures!";
 }
 
@@ -306,12 +295,12 @@ if($p[1] == "semporna-archipelago") {
 }
 
 if($p[1] == "semporna-seaventures-dive-rig") {
-	$t = "Seaventures Dive Rig, Best Diving Packages in Sabah";
+	$t = "Semporna Archipelago | Seaventures Dive Rig, Best Diving Packages in Sabah";
 	$d = "Book amazing borneo holidays with Sticky Rice. Best Diving packages and diving Resort in Malaysia. Dive Mabul, Kapalai and Sipadan. Meaningful Adventures";
 }
 
 if($p[1] == "semporna-mataking-resort") {
-	$t = "Mataking Resort, Malaysian Resorts, island and beaches";
+	$t = "Semporna Archipelago | Mataking Resort, Malaysian Resorts, island and beaches";
 	$d = "Book amazing borneo holidays with Sticky Rice. Great staff, beautiful villas, mesmerizing aquamarine and white powder sand beaches. Meaningful Adventures";	
 }
 
@@ -323,22 +312,22 @@ if($p[1] == "kinabalu-national-park") {
 }
 
 if($p[1] == "kinabalu-national-park-camping") {
-	$t = "Mt Kinabalu Camping, Amazing Borneo Tours";
+	$t = "Kinabalu National Park | Mt Kinabalu Camping, Amazing Borneo Tours";
 	$d = "Book amazing borneo holidays with Sticky Rice. Trekking, swim in waterfall, search for reptiles, amphibians, flora, fauna and more. Meaningful Adventures";
 }
 
 if($p[1] == "kinabalu-national-park-mountain-torq-ferrata") {
-	$t = "Mountain Torq via Ferrata, Mount Kinabalu Package";
+	$t = "Kinabalu National Park | Mountain Torq via Ferrata, Mount Kinabalu Package";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Mount Kinabalu, Mountain Torq via Ferrata Itinerary all inclusive package. Meaningful Adventures!";	
 }
 
 if($p[1] == "kinabalu-national-park-mount-kinabalu-climb") {
-	$t = "Mt Kinabalu Package, Mount Kinabalu Packages";
+	$t = "Kinabalu National Park | Mt Kinabalu Package, Mount Kinabalu Packages";
 	$d = "Book amazing borneo holidays with Sticky Rice. Mt Kinabalu Climbing Itinerary with transport from kota kinabalu to kinabalu park. Meaningful Adventures!";	
 }
 
 if($p[1] == "kinabalu-national-park-day-trip") {
-	$t = "Kinabalu National Park Day Trip";
+	$t = "Kinabalu National Park | Day Trip";
 	$d = "Book amazing borneo holidays with Sticky Rice. Mt Kinabalu Climbing Itinerary with transport from kota kinabalu to kinabalu park. Meaningful Adventures!";	
 }
 
@@ -350,12 +339,12 @@ if($p[1] == "kota-belud") {
 }
 
 if($p[1] == "kota-belud-kampung-tambatuon") {
-	$t = "Tambatuon Village, Sabah Holiday Package and Tourism";
+	$t = "Kota Belud | Tambatuon Village, Sabah Holiday Package and Tourism";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-belud-manana-borneo") {
-	$t = "Manana Borneo, Sabah Holiday Package and Tourism";
+	$t = "Kota Belud | Manana Borneo, Sabah Holiday Package and Tourism";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
@@ -367,17 +356,17 @@ if($p[1] == "kudat") {
 }
 
 if($p[1] == "kudat-northern-island-exploration") {
-	$t = "Travel to borneo, Northern Island Exploration";
+	$t = "Kudat | Northern Island Exploration";
 	$d = "Book amazing borneo holidays with Sticky Rice. Different side of Borneo, 5 days of exploring untouched culture & pristine islands. Meaningful Adventures!";	
 }
 
 if($p[1] == "kudat-hibiscus-villa") {
-	$t = "Kudat Hibiscus Villa";
+	$t = "Kudat | Hibiscus Villa";
 	$d = "Book amazing borneo holidays with Sticky Rice. Different side of Borneo, 5 days of exploring untouched culture & pristine islands. Meaningful Adventures!";	
 }
 
 if($p[1] == "kudat-hibiscus-beach-retreat") {
-	$t = "Kudat Hibiscus Beach Retreat";
+	$t = "Kudat | Hibiscus Beach Retreat";
 	$d = "Book amazing borneo holidays with Sticky Rice. Different side of Borneo, 5 days of exploring untouched culture & pristine islands. Meaningful Adventures!";	
 }
 
@@ -389,52 +378,52 @@ if($p[1] == "kota-kinabalu") {
 }
 
 if($p[1] == "kota-kinabalu-kayak-and-snorkel-safari") {
-	$t = "Kota Kinabalu Kayak & Snorkel Safari";
+	$t = "Kota Kinabalu | Kayak & Snorkel Safari";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-diving-and-snorkeling") {
-	$t = "Kota Kinabalu Diving & Snorkeling";
+	$t = "Kota Kinabalu | Diving & Snorkeling";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-white-water-rafting") {
-	$t = "Kota Kinabalu White Water Rafting";
+	$t = "Kota Kinabalu | White Water Rafting";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-cycling-programs") {
-	$t = "Kota Kinabalu Cycling Programs";
+	$t = "Kota Kinabalu | Cycling Programs";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-easy-breezy-ride") {
-	$t = "Easy Breezy Ride";
+	$t = "Kota Kinabalu | Easy Breezy Ride";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-kiulu-fun-and-green") {
-	$t = "Kiulu Fun & Green";
+	$t = "Kota Kinabalu | Kiulu Fun & Green";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-highland-cool-and-chill") {
-	$t = "Highland Cool & Chill";
+	$t = "Kota Kinabalu | Highland Cool & Chill";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-momma-house-cooking-class") {
-	$t = "Kota Kinabalu Momma's House Cooking Class";
+	$t = "Kota Kinabalu | Momma's House Cooking Class";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-borneo-orchard-house") {
-	$t = "Kota Kinabalu Borneo Orchard House";
+	$t = "Kota Kinabalu | Borneo Orchard House";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
 if($p[1] == "kota-kinabalu-accommodation") {
-	$t = "Kota Kinabalu Accommodations";
+	$t = "Kota Kinabalu | Accommodations";
 	$d = "Book amazing borneo holidays with Sticky Rice. Discover Dusun Tribe & longhouse accommodation by the banks of the Kedamaian River. Meaningful Adventures!";	
 }
 
@@ -463,33 +452,6 @@ if($p[1] == "custom-trips") {
 	$d = "Book amazing borneo holidays with Sticky Rice. Plan a trip or even a group trip. No worries on question like help me plan a trip. Meaningful Adventures!";	
 }
 
-//Photographs
-
-if($p[1] == "photographs-of-borneo") {
-	$t = "Beach pictures, Borneo, Kinabalu, Sabah Photographs";
-	$d = "Book amazing borneo holidays with Sticky Rice. Photographs of Borneo Attractions, islands, beaches, natures and people of Borneo. Meaningful Adventures!";	
-}
-
-if($p[1] == "photo-galleries-fauna") {
-	$t = "Amazing photographs of fauna in Borneo";
-	$d = "Book amazing borneo holidays with Sticky Rice Travel. View Beautiful photographs of Borneo in our Fauna Gallery. Meaningful Adventures!";	
-}
-
-if($p[1] == "photo-galleries-flora") {
-	$t = "Amazing photographs of flora in Borneo";
-	$d = "Book amazing borneo holidays with Sticky Rice Travel. View Beautiful photographs of Borneo in our Flora Gallery. Meaningful Adventures!";	
-}
-
-if($p[1] == "photo-galleries-landscape-of-borneo") {
-	$t = "Amazing photographs of landscape in Borneo";
-	$d = "Book amazing borneo holidays with Sticky Rice Travel. View Beautiful photographs of landscape in our Lanscape Gallery. Meaningful Adventures!";	
-}
-
-if($p[1] == "photo-galleries-people-of-borneo") {
-	$t = "Amazing photographs of people in Borneo";
-	$d = "Book amazing borneo holidays with Sticky Rice Travel. View Beautiful photographs of people in our People of Borneo Gallery. Meaningful Adventures!";	
-}
-
 if($p[1] == "responsible-tourism") {
 	$t = "Responsible Tourism, eco travel packages";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. We're proud responsible tourism agency, environment, cultures, communities, conservation and more.";	
@@ -501,7 +463,7 @@ if($p[1] == "contact-us") {
 }
 
 if($p[1] == "booking") {
-	$t = "Make a booking with Sticky Rice Travel Agency";
+	$t = "Booking | Make a booking with Sticky Rice Travel Agency";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Get a meaningful adventures when you make booking for your trip to Borneo. Make a booking today!";	
 }
 
@@ -511,17 +473,22 @@ if($p[1] == "terms") {
 }
 
 if($p[1] == "customer-reviews") {
-	$t = "Read about Sticky Rice travel reviews, holiday reviews";
+	$t = "Customer Reviews | Read about Sticky Rice travel reviews, holiday reviews";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Read what our customer have to say about us. Let them convince you choose us. Make a booking today!";	
 }
 
 if($p[1] == "privacy-policy") {
-	$t = "What we collect from our site visitors";
+	$t = "Privacy Policy | What we collect from our site visitors";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Privacy Policy, read what information we collect from our site visitors. Meaningful Adventures!";	
 }
 
 if($p[1] == "our-friends") {
 	$t = "Friends of Sticky Rice Travel";
+	$d = "Book amazing borneo holidays with Sticky Rice Travel. Take a look at some of our friends, it is worth it. Let them be part of your trip in Borneo too.";	
+}
+
+if($p[1] == "404") {
+	$t = "Sticky Rice Travel";
 	$d = "Book amazing borneo holidays with Sticky Rice Travel. Take a look at some of our friends, it is worth it. Let them be part of your trip in Borneo too.";	
 }
 
