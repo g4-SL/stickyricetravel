93893adc582d02433af139f9bc2e33e2298253ee
g4-SL <slouise@live.com.my>
2015-11-26 15:28:18 +0800

    Completed new layout for Kota udat folder

--- a/our-adventures/kudat.php
+++ a/our-adventures/kudat.php
@@ -10,6 +10,8 @@
 			
 		<div class="container hd lh">
 
+			<div class="two columns"></div>
+
 			<div class="sixteen columns big_title">
 				<ul class="brc">
 					<li><a href="/home">Home</a></li>
@@ -17,40 +19,78 @@
 					<li>Kudat</li>
 				</ul>
 				<h1>Kudat</h1>
-			</div>	
-			
-			<div class="clear"></div>
-			
-			<div class="sixteen columns">
 				<p>
 					The Kudat Peninsula, about 190km north of Kota Kinabalu, is the heartland of the Rungus people and a major centre of Rungus culture. Kudat city itself is a quiet port and fairly unremarkable. However, the long stretches of quiet beaches towards the Tip of Borneo is a treasure trove of quaint Rungus fishing villages, mangrove area and exudes the kind of seaside sleepiness that keeps you lingering for more. You can easily spend days lounging on the beach on white powder sand, or explore the little trails and lanes on a rented bicycles. There are also other attractions such as the honeybee farm and gong-making village.
-				</p>		
-			</div>	
+				</p>
+			</div>		
 			
 			<div class="clear"></div>
 
-			<div class="eight columns">
-				<div class="pp2">
-					<a href="./our-adventures/kudat/hibiscus-beach-retreat" title="Hibiscus Beach Retreat"><img src="/c/adventures/hibiscus-beach-retreat.jpg" class="img1" alt="Hibiscus Beach Retreat" /></a>
-				</div>
-			</div>
+			<div class="one columns"></div>
 
-			<div class="eight columns">
-				<div class="pp2">
-					<a href="./our-adventures/kudat/hibiscus-villa" title="Hibiscus Villa"><img src="/c/adventures/hibiscus-villa.jpg" class="img1" alt="Hibiscus Villa" /></a>
+			<div class="six columns product_box">
+				<div class="img_boxing">
+					<a href="./our-adventures/kudat/hibiscus-beach-retreat">
+						<img src="/c/adventures/hibiscus-beach-retreat.jpg" class="img1" alt="Hibiscus Beach Retreat"/>
+						<h2>Hibiscus Beach Retreat</h2>
+						<div class="rating accommodation">
+							<h3>Dwelling</h3>
+						</div>
+						<div class="rating difficulty">
+							<h3>Difficulty</h3>
+						</div>
+						<div class="clear"></div>
+						<div class="category">
+							<h3>Category</h3>
+						</div>
+					</a>			
+				</div>
+			</div>		
+
+			<div class="six columns product_box">
+				<div class="img_boxing">
+					<a href="./our-adventures/kudat/hibiscus-villa">
+						<img src="/c/adventures/hibiscus-villa.jpg" class="img1" alt="Hibiscus Villa"/>
+						<h2>Hibiscus Villa</h2>
+						<div class="rating accommodation">
+							<h3>Dwelling</h3>
+						</div>
+						<div class="rating difficulty">
+							<h3>Difficulty</h3>
+						</div>
+						<div class="clear"></div>
+						<div class="category">
+							<h3>Category</h3>
+						</div>
+					</a>			
 				</div>
 			</div>	
 
-			<div class="clear"></div>
-
-			<div class="eight columns">
-				<div class="pp2 marginbottom">
-					<a href="./our-adventures/kudat/northern-island-exploration" title="Northern Island Exploration"><img src="/c/adventures/northern-island-exploration.jpg" class="img1" alt="Northern Island Exploration" /></a>
+			<div class="six columns product_box">
+				<div class="img_boxing">
+					<a href="./our-adventures/kudat/northern-island-exploration">
+						<img src="/c/adventures/northern-island-exploration.jpg" class="img1" alt="Northern Island Exploration"/>
+						<h2>Northern Island Exploration</h2>
+						<div class="rating accommodation">
+							<h3>Dwelling</h3>
+						</div>
+						<div class="rating difficulty">
+							<h3>Difficulty</h3>
+						</div>
+						<div class="clear"></div>
+						<div class="category">
+							<h3>Category</h3>
+						</div>
+					</a>			
 				</div>
-			</div>
-
-			<div class="eight columns"></div>	
+			</div>	
+			
+			<div class="clear"></div>
 			
 		</div>
 		
 	</section>
+
+<script>
+	getDetails("Kudat");
+</script>
--- a/our-adventures/kudat/hibiscus-beach-retreat.php
+++ a/our-adventures/kudat/hibiscus-beach-retreat.php
@@ -11,6 +11,10 @@
 			
 		<div class="container hd">
 
+			<div class="four columns book_btn">
+				<?php include './a/side-book-now.php'; ?>   
+			</div>
+
 			<div class="sixteen columns big_title">
 				<ul class="brc">
 					<li><a href="./home">Home</a></li>
@@ -22,53 +26,60 @@
 			</div>	
 			
 			<div class="clear"></div>
+
+			<div class="large_screen_merge">
+				<div class="four columns">
+					<?php include './a/product-sidebar.php'; ?>
+				</div>
+
+				<div class="clear"></div>
 			
-			<div class="nine columns">
+				<div class="nine columns">
 
-				<p>
-					A sister lodge to the original Hibiscus Villa, Hibiscus Beach Retreat is a self-contained one-bedroom chalet right on the beach, perfect for couples and small families looking for privacy at an absolutely stunning and exclusive location. 
-				</p>				
-				<p>
-					Perch on a small cliff top with views of the South China Sea, wake up to an amazing view of the ocean. After breakfast, walk down steps to the beach – go for a swim, snorkel or stroll, or simply siesta in the hammock or sunbathe on the deck of the chalet.  
-				</p>
-				<p>
-					You can explore the numerous beaches and bays, walk to the Tip of Borneo, try your skills at fishing, go snorkeling, or even try a trek through the forest areas nearby. It is possible to pre-arrange a range of activities during your stay at the villa including mangrove river cruise, scuba diving, fishing or boat trips, jungle trekking including survival course, visit the Rungus museum where the traditions and culture of the local Rungus people can be explained, or a day out with a local in their farms – rice planting, rubber tapping, foreshore seafood foraging, rice de-husking, collecting coconuts, fishing with hand-nets. However, bear in mind that these activities are seasonal and dependent on the locals being able to take you with them. If all that sounds too stressful for you, massage and spa treatments are also available upon requests.  
-				</p>	
+					<p>
+						A sister lodge to the original Hibiscus Villa, Hibiscus Beach Retreat is a self-contained one-bedroom chalet right on the beach, perfect for couples and small families looking for privacy at an absolutely stunning and exclusive location. 
+					</p>				
+					<p>
+						Perch on a small cliff top with views of the South China Sea, wake up to an amazing view of the ocean. After breakfast, walk down steps to the beach – go for a swim, snorkel or stroll, or simply siesta in the hammock or sunbathe on the deck of the chalet.  
+					</p>
+					<p>
+						You can explore the numerous beaches and bays, walk to the Tip of Borneo, try your skills at fishing, go snorkeling, or even try a trek through the forest areas nearby. It is possible to pre-arrange a range of activities during your stay at the villa including mangrove river cruise, scuba diving, fishing or boat trips, jungle trekking including survival course, visit the Rungus museum where the traditions and culture of the local Rungus people can be explained, or a day out with a local in their farms – rice planting, rubber tapping, foreshore seafood foraging, rice de-husking, collecting coconuts, fishing with hand-nets. However, bear in mind that these activities are seasonal and dependent on the locals being able to take you with them. If all that sounds too stressful for you, massage and spa treatments are also available upon requests.  
+					</p>	
 
-				<h2 class="grunge5">Accommodation...</h2>
-				<table class="tbl_p">
-					<tr>
-						<td class="he">Accommodation</td>
-						<td>		
-							1 bedroom chalet Queen-sized bed with occupancy of 2 adults and maximum 2 additional adults/kids, en-suite toilet and shower.
-						</td>
-					</tr>
-					<tr>
-						<td class="he">Food</td>
-						<td>
-							<i>Daily breakfast included. Other meals can be self-catered for.</i>
-							<br /><br />
-							Drinking water, tea, coffee, milk and sugar are provided throughout. F&amp;B ordering service before your stay and takeaway service for lunch/dinner. There are several restaurants at the nearby Tip of Borneo and in Kudat.
-						</td>
-					</tr>
-					<tr>
-						<td class="he">Facilities / Service</td>
-						<td>
-							Daily maid service, daily continental breakfast, 24 hour security, BBQs prepared by local chef upon request, air-conditioning and ceiling fan, stereo with iPod dock, coffee machine, snorkeling equipment, sun loungers, TV and DVD player, fridge, toaster and kettle, sofa-bed. Note that Wi-Fi and mobile phone signal are not available at the retreat.
-						</td>
-					</tr>
-					<tr>
-						<td class="he">Location</td>
-						<td>
-							Hibiscus Beach Retreat is in a secluded position within the Kudat Peninsula, about 170km northeast of Kota Kinabalu. The nearest town Kudat is a 30 minutes drive away, and the Tip of Borneo is a short 5 minutes drive.
-						</td>
-					</tr>
-				</table>
-				
+					<h2 class="grunge5">Accommodation...</h2>
+					<table class="tbl_p">
+						<tr>
+							<td class="he">Accommodation</td>
+							<td>		
+								1 bedroom chalet Queen-sized bed with occupancy of 2 adults and maximum 2 additional adults/kids, en-suite toilet and shower.
+							</td>
+						</tr>
+						<tr>
+							<td class="he">Food</td>
+							<td>
+								<i>Daily breakfast included. Other meals can be self-catered for.</i>
+								<br /><br />
+								Drinking water, tea, coffee, milk and sugar are provided throughout. F&amp;B ordering service before your stay and takeaway service for lunch/dinner. There are several restaurants at the nearby Tip of Borneo and in Kudat.
+							</td>
+						</tr>
+						<tr>
+							<td class="he">Facilities / Service</td>
+							<td>
+								Daily maid service, daily continental breakfast, 24 hour security, BBQs prepared by local chef upon request, air-conditioning and ceiling fan, stereo with iPod dock, coffee machine, snorkeling equipment, sun loungers, TV and DVD player, fridge, toaster and kettle, sofa-bed. Note that Wi-Fi and mobile phone signal are not available at the retreat.
+							</td>
+						</tr>
+						<tr>
+							<td class="he">Location</td>
+							<td>
+								Hibiscus Beach Retreat is in a secluded position within the Kudat Peninsula, about 170km northeast of Kota Kinabalu. The nearest town Kudat is a 30 minutes drive away, and the Tip of Borneo is a short 5 minutes drive.
+							</td>
+						</tr>
+					</table>
+				</div>
 			</div>	
 			
 			<div class="seven columns bp">
-				<?php include './a/side-book-now.php'; ?>
+				<div class="book_btn"><?php include './a/side-book-now.php'; ?></div>
 				<img src="c/adventures-hibiscus-beach-retreat/1-400.jpg" alt="Hibiscus Beach Retreat" />
 				<img src="c/adventures-hibiscus-beach-retreat/2-400.jpg" alt="Hibiscus Beach Retreat" />
 				<img src="c/adventures-hibiscus-beach-retreat/3-400.jpg" alt="Hibiscus Beach Retreat" />
@@ -81,3 +92,7 @@
 		</div>
 		
 	</section>
+
+<script>
+	populateSidebar("Kudat", "Hibiscus Beach Retreat");
+</script>
